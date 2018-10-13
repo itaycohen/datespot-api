4f1106d9d9544ad75faa17c6396fd148a3d1a776
IC <itay2002@gmail.com>
2018-10-13 16:48:32 +0300

    server port change

--- server.js
+++ server.js
@@ -58,7 +58,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
